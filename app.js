54f4d804e30842a75020f1ee7cf87b4260115a4f
Malik Umair Khalid <fighteromega@gmail.com>
2021-08-06 11:46:57 +0500

    Drum Kit Assignment Completed

---
+++ app.js
@@ -0,0 +1,17 @@
+
+window.addEventListener("keydown" , playaudio)
+function playaudio(e){
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"`)
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`)
+    if(!audio) {return}; 
+    audio.play()
+    audio.currentTime = 0 ;
+    key.classList.add("playing")
+}
+let keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener("transitionend" , removeTransition))
+
+function removeTransition(e){
+    if(e.propertyName !== "transform"){return}
+    this.classList.remove("playing")
+}
